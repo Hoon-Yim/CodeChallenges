6895e1d1be5cc1733b2003f32d7b0b54e370d665
Hoon Yim <yimsh4507@gmail.com>
2024-04-09 02:17:13 -0400

    Updated problem 169

--- Easy/169-majority-element.cpp
+++ Easy/169-majority-element.cpp
@@ -1,24 +1,45 @@
 #include <vector>
 #include <cmath>
+#include <unordered_map>
+#include <utility>
 
 class Solution
 {
 public:
     int majorityElement(std::vector<int>& nums)
     {
-        int count = 0, biggest = 0;
+        std::unordered_map<int, int> map;
 
-        for (int num : nums)
+        for (const int num : nums)
         {
-            if (count == 0)
-            {
-                biggest = num;
-            }
-
-            if (num == biggest) count++;
-            else count--;
+            if (map.find(num) == map.end()) map[num] = 0;
+            else map[num] += 1;
         }
 
-        return biggest;
+        std::pair<int, int> maxEntry = *map.begin();
+        for (const auto &entry : map)
+        {
+            if (entry.second > maxEntry.second) maxEntry = entry;
+        }
+
+        return maxEntry.first;
     }
+
+//    int majorityElement(std::vector<int>& nums)
+//    {
+//        int count = 0, biggest = 0;
+//
+//        for (int num : nums)
+//        {
+//            if (count == 0)
+//            {
+//                biggest = num;
+//            }
+//
+//            if (num == biggest) count++;
+//            else count--;
+//        }
+//
+//        return biggest;
+//    }
 };
